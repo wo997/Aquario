c22908757847aab5714f55d3420c226be5be0c8c
wo997wo997 <wojtekwo997@gmail.com>
2018-06-09 00:28:03 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){ //ładuje index.html zamiast domyślnie
 
 app.use('/htdocs', express.static(__dirname+'/htdocs'));
 
-var HOWMANYSLOTS = 10;
+const HOWMANYSLOTS = 10;
 
 var SLOTS = [];
 for (i=0;i<HOWMANYSLOTS;i++) SLOTS.push(false);
@@ -27,7 +27,10 @@ SHARK.push([6000,500,5,3,0,1]);
 SHARK.push([8000,500,5,3,0,1]);
 SHARK.push([1500,300,5,3,0,1]);
 
-var BOTTOM = [], BOTTOMSIZE = 50, BOUNDRIGHT = 15000, BOTTOMKEY;
+var BOTTOM = [];
+const BOTTOMSIZE = 50;
+const BOUNDRIGHT = 13000;
+var BOTTOMKEY;
 
 var time = 0;
 
@@ -258,18 +261,18 @@ function HANDLEGAME()
 		
 		var fish = FISH[i];
 		var dir = fish[4];
-		FX = fish[0];
-		FY = fish[1];
-		SIN = Math.sin(dir);
-		COS = Math.cos(dir);
-		SIZE = getSize(fish[2]);
-		VEL = fish[3];
-		FISH[i][0] += VEL*COS;
-		FISH[i][1] += VEL*SIN;
+		var FX = fish[0];
+		var FY = fish[1];
+		var SIN = Math.sin(dir);
+		var COS = Math.cos(dir);
+		var SIZE = getSize(fish[2]);
+		var VEL = fish[3];
+		FISH[i][0] += VEL*COS*2;
+		FISH[i][1] += VEL*SIN*2;
 		var mouthX = FX + COS*SIZE*18;
 		var mouthY = FY + SIN*SIZE*18;
 		
-		FISH[i][2] -= 0.00001*(SIZE-0.7)*(200-FISH[i][9]);
+		FISH[i][2] -= 0.00004*(SIZE-0.7)*(120-FISH[i][9]);
 		
 		/*for (a=0;a<PLANKTON.length;a++)
 		{
@@ -287,13 +290,13 @@ function HANDLEGAME()
 			var dy = FISH[a][1]-mouthY;
 			var s2 = getSize(FISH[a][2]);
 			
-			if (dx*dx+dy*dy < squared(SIZE*8+s2*18))
+			if (dx*dx+dy*dy < squared(SIZE*8+s2*19))
 			{
-				var ratio = 0.03*Math.sqrt(SIZE/s2);
+				var ratio = 0.07*Math.sqrt(SIZE/s2);
 				gain(i,ratio);
 				lose(a,ratio);
 				if (FISH[a][9] <= 0) gain(i,FISH[a][2]);
-				FISH[i][3] *= 0.95;
+				FISH[i][3] *= 0.9;
 			}
 		}
 		
@@ -303,15 +306,15 @@ function HANDLEGAME()
 			if (turn>1) turn = 1;
 			if (turn<-1) turn = -1;
 			
-			FISH[i][4] += turn/(10+5*SIZE);
+			FISH[i][4] += turn/(5+2*SIZE);
 			
-			if (FISH[i][7] && FISH[i][10]>0.5)
+			if (FISH[i][7] && FISH[i][10]>=1)
 			{
-				FISH[i][3] += 0.2;
-				FISH[i][10] -= 0.5;
+				FISH[i][3] += 0.4;
+				FISH[i][10] -= 1;
 			}
 		}
-		FISH[i][10] += 0.07;
+		FISH[i][10] += 0.2;
 		if (FISH[i][10]>100) FISH[i][10] = 100;
 		
 		var how = mod(FX/BOTTOMSIZE,1);
@@ -321,7 +324,7 @@ function HANDLEGAME()
 		if (FY > bottom)
 		{
 			var much = (FY - bottom)*0.3;
-			FISH[i][3] *= 1-0.01*much;
+			FISH[i][3] *= 1-0.02*much;
 			FISH[i][0] += much * (ri-le)/BOTTOMSIZE;
 			FISH[i][1] -= much;
 			
@@ -329,9 +332,9 @@ function HANDLEGAME()
 			if (much>0)
 			{
 				if (much>5) much = 5;
-				FISH[i][4] += SIN * (COS<0?1:-1) * 0.02 * much;
-				FISH[i][3] *= 0.97;
-				FISH[i][9] -= 0.5;
+				FISH[i][4] += SIN * (COS<0?1:-1) * 0.04 * much;
+				FISH[i][3] *= 0.94;
+				FISH[i][9] -= 2;
 			}
 		}
 		else if (FY < SIZE * 6)
@@ -340,12 +343,12 @@ function HANDLEGAME()
 			if (much>0)
 			{
 				if (much>5) much = 5;
-				FISH[i][4] += much * (COS>0?0.02:-0.02)/(Math.abs(VEL)+0.1);
-				FISH[i][3] += much * SIN * 0.025;
-				FISH[i][1] += 0.1;
+				FISH[i][4] += much * (COS>0?0.04:-0.04)/(Math.abs(VEL)+0.1);
+				FISH[i][3] += much * SIN * 0.05;
+				FISH[i][1] += 0.2;
 			}
 		}
-		else FISH[i][3] = FISH[i][3]*0.9 + 0.25;
+		else FISH[i][3] = FISH[i][3]*0.81 + 0.5;
 		
 		if (FISH[i][9]<=0 || FISH[i][2]<1)
 		{
@@ -355,27 +358,27 @@ function HANDLEGAME()
 		}
 		else
 		{
-			FISH[i][9] += 0.03;
+			FISH[i][9] += 0.06;
 			if (FISH[i][9]>100) FISH[i][9] = 100;
 		}
 	}
 	
-	var len = 270 + 80 * Math.sin(time*0.01);
-	var shift = 150 + 80 * Math.sin(time*0.003427);
+	var len = 270 + 80 * Math.sin(time*0.02);
+	var shift = 150 + 80 * Math.sin(time*0.0066);
 	
 	for (i=0;i<SHARK.length;i++)
 	{
 		var fish = SHARK[i];
 		var vel = fish[3];
 		var dir = fish[4];
-		FX = fish[0];
-		FY = fish[1];
-		SIN = Math.sin(dir);
-		COS = Math.cos(dir);
-		SIZE = fish[2];
-		VEL = fish[3];
-		SHARK[i][0] = FX + vel*COS;
-		SHARK[i][1] = FY + vel*SIN;
+		var FX = fish[0];
+		var FY = fish[1];
+		var SIN = Math.sin(dir);
+		var COS = Math.cos(dir);
+		var SIZE = fish[2];
+		var VEL = fish[3];
+		SHARK[i][0] = FX + vel*COS*2;
+		SHARK[i][1] = FY + vel*SIN*2;
 		var mouthX = FX + COS*SIZE*18;
 		var mouthY = FY + SIN*SIZE*18;
 		
@@ -390,7 +393,7 @@ function HANDLEGAME()
 			var dist = dx*dx+dy*dy;
 			var s2 = getSize(FISH[a][2]);
 			
-			if (FY>30 && dist<60000 && Math.random()<0.1)
+			if (FY>30 && dist<60000 && Math.random()<0.15)
 			{
 				if (dist < followClosest)
 				{
@@ -408,7 +411,7 @@ function HANDLEGAME()
 			
 			if (dist<Math.pow(SIZE*2+s2,2)*25)
 			{
-				FISH[a][9] -= 3/(2+s2);
+				FISH[a][9] -= 6/(2+s2);
 			}
 		}
 		
@@ -418,7 +421,7 @@ function HANDLEGAME()
 			if (turn>1) turn = 1;
 			if (turn<-1) turn = -1;
 			
-			SHARK[i][4] += turn/(10+10*SIZE);
+			SHARK[i][4] += turn/(5+4*SIZE);
 		}
 		
 		var how = mod(FX/BOTTOMSIZE,1);
@@ -427,7 +430,7 @@ function HANDLEGAME()
 		var bottom = le*(1-how) + ri*how - SIZE * 6;
 		if (FY > bottom)
 		{
-			var much = (FY - bottom)*0.3;
+			var much = (FY - bottom)*0.6;
 			SHARK[i][3] *= 1-0.01*much;
 			SHARK[i][0] += much * (ri-le)/BOTTOMSIZE;
 			SHARK[i][1] -= much;
@@ -436,8 +439,8 @@ function HANDLEGAME()
 			if (much>0)
 			{
 				if (much>5) much = 5;
-				SHARK[i][4] += SIN * (COS<0?1:-1) * 0.02 * much;
-				SHARK[i][3] *= 0.97;
+				SHARK[i][4] += SIN * (COS<0?1:-1) * 0.04 * much;
+				SHARK[i][3] *= 0.94;
 				if (FX<BOUNDRIGHT/2) SHARK[i][0] += 500;
 				else SHARK[i][0] -= 500;
 			}
@@ -448,11 +451,11 @@ function HANDLEGAME()
 			if (much>0)
 			{
 				if (much>5) much = 5;
-				SHARK[i][4] += much * (COS>0?0.01:-0.01)/(Math.abs(VEL)+0.1);
-				SHARK[i][3] += much * SIN * 0.025;
+				SHARK[i][4] += much * (COS>0?0.04:-0.04)/(Math.abs(VEL)+0.1);
+				SHARK[i][3] += much * SIN * 0.05;
 			}
 		}
-		else SHARK[i][3] = SHARK[i][3]*0.9 + 0.25 + 0.1 * Math.sin(i+time*0.001);
+		else SHARK[i][3] = SHARK[i][3]*0.81 + 0.5 + 0.2 * Math.sin(i+time*0.002);
 		
 		SIN = Math.sin(SHARK[i][5]);
 		COS = Math.cos(SHARK[i][5]);
@@ -460,15 +463,15 @@ function HANDLEGAME()
 		var s1 = sensor(FX+COS*len+SIN*shift,FY+SIN*len-COS*shift);
 		var s2 = sensor(FX+COS*len-SIN*shift,FY+SIN*len+COS*shift);
 		
-		if (s1) SHARK[i][5] += 0.2;
-		if (s2) SHARK[i][5] -= 0.2;
+		if (s1) SHARK[i][5] += 0.4;
+		if (s2) SHARK[i][5] -= 0.4;
 		if (s1 && s2)
 		{
-			SHARK[i][3] *= 0.85;
+			SHARK[i][3] *= 0.72;
 			SHARK[i][5] += Math.PI;
 		}
-		if (!s1 && !s2) SHARK[i][5] += Math.random()*0.05-0.025;
-		if (Math.abs(Math.sin(SHARK[i][5]))<0.1) SHARK[i][5]+=(2*Math.round(Math.random())-1)*0.3;
+		if (!s1 && !s2) SHARK[i][5] += Math.random()*0.1-0.05;
+		if (Math.abs(Math.sin(SHARK[i][5]))<0.1) SHARK[i][5]+=(2*Math.round(Math.random())-1)*0.6;
 	}
 	
 	PLAYERS.data = FISH;
@@ -477,17 +480,18 @@ function HANDLEGAME()
 	var d = new Date();
 	var delay = 1000*d.getSeconds()+d.getMilliseconds();
 	
-	if (once<1) once++;
+	/*if (once<1) once++;
 	else
 	{
 		once = 0;
 		io.emit('updatePlayers',PLAYERS,SENDSHARKS,delay);
-	}
-	setTimeout(HANDLEGAME,1000/60);
-	// FPS SET TO 30 WORKS AS WELL JUST MULTIPLY DIRECTION AND VELOCITY BY A CONST FACTOR EASY BRO
+	}*/
+	io.emit('updatePlayers',PLAYERS,SENDSHARKS,delay);
+	
+	setTimeout(HANDLEGAME,1000/30);
 }
 
-var once = 0; // skip 1/2
+//var once = 0; // skip 1/2
 
 function squared(n)
 {
@@ -510,8 +514,8 @@ function mod(a,b)
 CREATELEVEL();
 HANDLEGAME();
 
-var port = process.env.PORT || 8080; // heroku
-//var port = 80;
+//var port = process.env.PORT || 8080; // heroku
+var port = 80;
 
 http.listen(port, function(){ //nasluchuje
   console.log('listening on *:80');
